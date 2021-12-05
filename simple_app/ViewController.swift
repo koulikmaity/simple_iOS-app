a3a518fffaad3b7dff163b8f20a25529b525e10a
Koulik <maitykoulik2003@gmail.com>
2021-12-05 11:51:23 +0530

    Initial Commit

---
+++ simple_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_app
+//
+//  Created by Koulik Maity on 05/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
